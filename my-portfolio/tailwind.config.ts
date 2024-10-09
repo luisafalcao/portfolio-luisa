71a63a5c55a3c2f29758cf58f2685cde0ae7c6f1
Luisa Falcão <luisafalcaovaz@gmail.com>
2024-10-09 18:07:37 -0300

    Fontes instaladas

--- my-portfolio/tailwind.config.ts
+++ my-portfolio/tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        primary: ["var(--font-montserrat)"],
+        secondary: ["var(--font-source-code-pro)"]
+      }
     },
   },
   plugins: [],
