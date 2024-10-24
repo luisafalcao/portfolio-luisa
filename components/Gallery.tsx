cc20976a4afdbd7a745c45be65c2906a6b1f0f86
Luisa Falcão <luisafalcaovaz@gmail.com>
2024-10-24 19:35:40 -0300

    Alteracoes na gallery

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -5,22 +5,23 @@ import clsx from "clsx"
 export default function Gallery({ className, mainImage, gallery }: GalleryProps) {
     return (
         <div
-            className={clsx("w-full overflow-x-hidden grid grid-cols-2 grid-rows-2 max-w-screen-2xl", className)}
+            className={clsx("w-full overflow-x-hidden flex flex-col max-w-screen-2xl", className)}
         >
-            <div className="relative w-full col-span-2 h-[40vh] md:h-[50vh]">
-                <PrismicNextImage field={mainImage} quality={100} className="object-cover h-full" />
+            {/* row 1 */}
+            <div className="bg-red-200 basis-1/2 max-h-[50vh]">
+                <PrismicNextImage placeholder="blur" blurDataURL={mainImage?.url ?? undefined} field={mainImage} quality={100} className="object-cover h-full" />
             </div>
 
-            <div className="col-span-2 flex snap-x snap-mandatory overflow-x-scroll h-[40vh] md:h-[50vh]">
+            {/* row 2 */}
+            <div className="flex basis-1/2 overflow-x-scroll snap-x snap-mandatory max-h-[50vh]">
                 {
                     gallery?.map((item, index: number) => (
-                        <div key={index} className="basis-1/2 shrink-0 snap-start relative">
-                            <PrismicNextImage fill placeholder="blur" blurDataURL={item.photo?.url ?? undefined} field={item.photo} width={1000} height={1000} quality={100} style={{ objectFit: "cover", height: "100%" }} />
-                        </div>
+                        <PrismicNextImage key={index} placeholder="blur" blurDataURL={item.photo?.url ?? undefined} field={item.photo} height={1000} quality={100} className="h-full w-auto shrink-0 snap-start relative object-cover" />
                     ))
                 }
 
             </div>
+
         </div>
     )
 }
