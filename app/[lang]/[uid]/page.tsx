ef81869f89ca2d083e2c779820ce8cbde1809205
Luisa Falcão <luisafalcaovaz@gmail.com>
2025-01-21 20:24:18 -0300

    Revert "Tirei filtro duplicado do page.tsx dos projects"
    This reverts commit 1fceb6df8f57a7935464f8d09b1ce31e023684b8.

--- app/[lang]/[uid]/page.tsx
+++ app/[lang]/[uid]/page.tsx
@@ -1,4 +1,4 @@
-import { PrismicDocument } from "@prismicio/client";
+import { PrismicDocument, filter } from "@prismicio/client";
 import { SliceZone } from "@prismicio/react";
 import { createClient } from "@/prismicio";
 import { Metadata } from "next";
@@ -10,19 +10,21 @@ import { LanguageSwitcher } from "@/components/LanguageSwitcher";
 
 export default async function Page({ params }: { params: Params }) {
     const client = createClient();
-
     const currentPage: PrismicDocument = await client
         .getByUID("project", params.uid, { lang: params.lang })
         .catch(() => notFound());
-
     const locales = await getLocales(currentPage, client);
+    const pages = await client.getAllByType("project", {
+        predicates: [filter.not('my.project.uid', 'homepage')],
+        lang: '*'
+    });
 
-    const pages = await client.getAllByType("project", { lang: '*' });
+    const projectPages = pages.filter((item, index, self) => index === self.findIndex((t) => t.uid === item.uid));
 
     return (
         <>
             <LanguageSwitcher locales={locales} />
-            <SliceZone slices={currentPage.data.slices} components={components} context={{ pages, currentPage }} />
+            <SliceZone slices={currentPage.data.slices} components={components} context={{ projectPages, currentPage }} />
         </>
     )
 }
@@ -41,11 +43,12 @@ export async function generateMetadata({ params }: { params: Params }): Promise<
 
 export async function generateStaticParams() {
     const client = createClient();
-    const projectPages = await client.getAllByType("journal_entry", { lang: '*' });
+    const projectPages = await client.getAllByType("project", {
+        predicates: [filter.not('my.project.uid', 'homepage')],
+        lang: '*'
+    });
 
-    const filteredPages = projectPages.filter((page) => page.uid !== "homepage");
-
-    return filteredPages.map((page) => {
+    return projectPages.map((page) => {
         return { uid: page.uid, lang: page.lang };
     });
 }
