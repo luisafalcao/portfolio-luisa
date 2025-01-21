864253b0d5e03c4b29f2e1d2c52f66be19f37665
Luisa Falcão <luisafalcaovaz@gmail.com>
2025-01-21 20:25:50 -0300

    Revert "Alteração pra melhorar LCP"
    This reverts commit a85843cef610c0c156764a3143a893362e48d98a.

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -8,14 +8,14 @@ export default function Gallery({ className, mainImage, gallery }: GalleryProps)
         <div className={clsx("w-full overflow-x-hidden flex flex-col max-w-screen-2xl", className)}>
             {/* row 1 */}
             <div className="basis-1/2 md:max-h-[50vh]">
-                <PrismicNextImage priority placeholder="blur" blurDataURL={mainImage?.url ?? undefined} field={mainImage} quality={100} className="object-cover h-full" />
+                <PrismicNextImage placeholder="blur" blurDataURL={mainImage?.url ?? undefined} field={mainImage} quality={100} className="object-cover h-full" />
             </div>
 
             {/* row 2 */}
             <div className="relative basis-1/2 md:max-h-[50vh]">
                 <div id="scrollable-gallery" className="flex overflow-x-scroll snap-x snap-mandatory ">
                     {gallery?.map((item, index) => (
-                        <PrismicNextImage priority key={index} placeholder="blur" blurDataURL={item.photo?.url ?? undefined} field={item.photo} height={1000} quality={100} className="h-auto w-auto shrink-0 snap-start object-cover" />
+                        <PrismicNextImage key={index} placeholder="blur" blurDataURL={item.photo?.url ?? undefined} field={item.photo} height={1000} quality={100} className="h-auto w-auto shrink-0 snap-start object-cover" />
                     ))}
                 </div>
                 <Scroller /> {/* Place Scroller within the same relative container */}
